fb75d21491d49dfed055ae5c43b6e76dbc15e9d0
lefline <issamkharbache2@gmail.com>
2024-05-31 19:48:27 +0100

    wrapp register seller with suspense

--- app/(frontend)/reset-password/page.jsx
+++ app/(frontend)/reset-password/page.jsx
@@ -1,18 +1,21 @@
 import ResetPasswordForm from "@/components/frontend/authentication/ResetPasswordForm";
+import { Suspense } from "react";
 
 export default function PasswordReset() {
   return (
-    <section>
-      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <div className="w-full bg-slate-200 rounded-lg shadow-lg dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white text-center">
-              Reset Password
-            </h1>
-            <ResetPasswordForm />
+    <Suspense>
+      <section>
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+          <div className="w-full bg-slate-200 rounded-lg shadow-lg dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white text-center">
+                Reset Password
+              </h1>
+              <ResetPasswordForm />
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </Suspense>
   );
 }
